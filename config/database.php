85e5188a8a9309285aa542bbe92baa7b0e2b8c99
cdyun <121625706@qq.com>
2026-03-15 13:15:54 +0800

    cdyun

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 
 return [
     // 默认使用的数据库连接配置
-    'default'         => env('database.type', 'mysql'),
+    'default'         => env('mysql.type', 'mysql'),
 
     // 自定义时间查询规则
     'time_query_rule' => [],
@@ -23,23 +23,23 @@ return [
     'connections'     => [
         'mysql' => [
             // 数据库类型
-            'type'            => env('database.type', 'mysql'),
+            'type'            => env('mysql.type', 'mysql'),
             // 服务器地址
-            'hostname'        => env('database.hostname', '127.0.0.1'),
+            'hostname'        => env('mysql.hostname', '127.0.0.1'),
             // 数据库名
-            'database'        => env('database.database', ''),
+            'database'        => env('mysql.database', ''),
             // 用户名
-            'username'        => env('database.username', 'root'),
+            'username'        => env('mysql.username', 'root'),
             // 密码
-            'password'        => env('database.password', ''),
+            'password'        => env('mysql.password', ''),
             // 端口
-            'hostport'        => env('database.hostport', '3306'),
+            'hostport'        => env('mysql.hostport', '3306'),
             // 数据库连接参数
             'params'          => [],
             // 数据库编码
-            'charset'         => env('database.charset', 'utf8mb4'),
+            'charset'         => env('mysql.charset', 'utf8mb4'),
             // 数据库表前缀
-            'prefix'          => env('database.prefix', ''),
+            'prefix'          => env('mysql.prefix', ''),
 
             // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
             'deploy'          => 0,
